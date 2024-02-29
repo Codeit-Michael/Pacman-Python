6329d21a1e613a61452ba94cbe29c5955dda29b9
Codeit-Michael <michaelmaranan030@gmail.com>
2024-03-01 01:02:49 +0800

    change ghost movement

--- ghost.py
+++ ghost.py
@@ -2,7 +2,7 @@ import pygame
 import random
 import time
 
-from settings import CHAR_SIZE, PLAYER_SPEED
+from settings import WIDTH, CHAR_SIZE, PLAYER_SPEED
 
 class Ghost(pygame.sprite.Sprite):
 	def __init__(self, row, col):
@@ -15,14 +15,31 @@ class Ghost(pygame.sprite.Sprite):
 		self.color = pygame.Color("gray48")
 		self.move_directions = [(-1,0), (0,-1), (1,0), (0,1)]
 
-	def update(self, screen, wall_id_list):
-		while True:
-			direction = random.choice(self.move_directions)
-			curr_x, curr_y = self.rect.x // CHAR_SIZE, self.rect.y // CHAR_SIZE
-			new_id = (curr_x + direction[0], curr_y  + direction[1])
-			if new_id not in wall_id_list:
-				self.rect.move_ip(direction)
+		self.directions = {'left': (-PLAYER_SPEED, 0), 'right': (PLAYER_SPEED, 0), 'up': (0, -PLAYER_SPEED), 'down': (0, PLAYER_SPEED)}
+		# self.keys = {'left': pygame.K_LEFT, 'right': pygame.K_RIGHT, 'up': pygame.K_UP, 'down': pygame.K_DOWN}
+		self.keys = ['left', 'right', 'up', 'down']
+		self.direction = (0, 0)
 
-				pygame.draw.rect(screen, self.color, self.rect)
-				break	
 
+	def is_collide(self, x, y, walls_collide_list):
+		tmp_rect = self.rect.move(x, y)
+		if tmp_rect.collidelist(walls_collide_list) == -1:
+			return False
+		return True
+
+
+	def update(self, screen, walls_collide_list):
+		# ghost movement
+		random_move = random.choice(self.keys)
+		if not self.is_collide(*self.directions[random_move], walls_collide_list):
+			self.direction = self.directions[random_move]
+		if not self.is_collide(*self.direction, walls_collide_list):
+			self.rect.move_ip(self.direction)
+
+		# teleporting to the other side of the map
+		if self.rect.right <= 0:
+			self.rect.x = WIDTH
+		elif self.rect.left >= WIDTH:
+			self.rect.x = 0
+
+		pygame.draw.rect(screen, self.color, self.rect)
