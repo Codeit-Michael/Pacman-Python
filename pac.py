8902d6bdde17a139e315c836d88aa3e94f384891
Codeit-Michael <michaelmaranan030@gmail.com>
2024-04-25 12:26:07 +0800

    update assets

--- pac.py
+++ pac.py
@@ -9,8 +9,8 @@ class Pac(pygame.sprite.Sprite):
 		self.abs_x = (row * CHAR_SIZE)
 		self.abs_y = (col * CHAR_SIZE)
 
-		# pac info 
-		# img_path = 'assets/pac/pac.png'
+		# pac animation
+		# img_path = 'assets/pac/idle/0.png'
 		# self.image = pygame.image.load(img_path)
 		# self.image = pygame.transform.scale(self.image, (CHAR_SIZE, CHAR_SIZE))
 		# self.rect = self.image.get_rect(topleft = pos)
@@ -25,11 +25,9 @@ class Pac(pygame.sprite.Sprite):
 		self.life = 3
 		self.pac_score = 0
 
-
 	def move_to_start_pos(self):
 		self.rect.x = self.abs_x
 		self.rect.y = self.abs_y
-
 
 	def update(self, screen):
 		pygame.draw.rect(screen, self.color, self.rect)
